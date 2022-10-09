e96b9bb132edc6533da08c21f6c6eee29c8614ee
Vedrrox <98419540+Vedrrox@users.noreply.github.com>
2022-10-09 12:28:12 +0200

    Update main.c
    zamiana małych liter na duże

--- lab2/main.c
+++ lab2/main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
-			printf("%s",param);
-		}else
-			putchar(format_string[i]);
-	}
-	puts("");
-	return 0;
-}
+	int my_printf(char *format_string, char *param) {
+    
+    for (int i = 0; i < strlen(format_string); i++){
+          if (isalpha(format_string[i])) {
+            if (islower(format_string[i]))
+                format_string[i] = (char) toupper(format_string[i]);
+            else
+                format_string[i] = (char) tolower(format_string[i]);
+        }
+    }
+    
+        
 
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
-		gets(buf2);
-		my_printf(buf,buf2);
+    for (int i = 0; i < strlen(format_string); i++) {
+
+            if ((format_string[i] == '#') && (format_string[i + 1] == 'k')) {
+                i++;
+                printf("%s", param);
+            } else
+                putchar(format_string[i]);
+        }
+        
+        puts("");
+        return 0;
+		
+    }
+
+    int main(int argc, char *argv[]) {
+        char buf[1024], buf2[1024];
+        while (gets(buf)) {
+            gets(buf2);
+            my_printf(buf, buf2);
+        }
+        return 0;
 	}
-	return 0;
-}
+    
