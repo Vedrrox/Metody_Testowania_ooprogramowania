0b91e2d937c37437c5f8274a43c0c4e58cf87146
Vedrrox <225344@edu.p.lodz.pl>
2022-11-06 11:44:01 +0100

    python

---
+++ lab4/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+def my_printf(format_string,param):
+    #print(format_string)
+    shouldDo=True
+    for idx in range(0,len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+                print(param,end="")
+                shouldDo=False
+            else:
+                print(format_string[idx],end="")
+        else:
+            shouldDo=True
+    print("")
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
