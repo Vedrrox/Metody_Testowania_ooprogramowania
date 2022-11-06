56d1662109efbabcf4f959bbcf71b1fbe8bdbedc
Vedrrox <225344@edu.p.lodz.pl>
2022-11-06 12:04:23 +0100

    dodanie testów i nowych funkcji

--- lab4/main.py
+++ lab4/main.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 
 import sys
+def print_digits(format_string,index):
+    buffer=""
+    for i in range (index,len(format_string)):
+        if format_string[i].isdigit():
+            buffer += format_string[i]
+        elif format_string[i]!=" ":
+            return ""
+        else:
+            return buffer[::-1]   
+    return ""
+
 
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    buffer=""
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+            if format_string[idx] == '#' and format_string[idx+1]=='g':
                 print(param,end="")
-                shouldDo=False
+                buffer=print_digits(format_string,idx+3)
+                if(buffer!=""):
+                    print(" "+buffer,end="")
+                shouldDo=False     
             else:
-                print(format_string[idx],end="")
+                if (buffer==""):
+                    print(format_string[idx],end="")
         else:
+            buffer==""
             shouldDo=True
     print("")
+
+
+
 
 data=sys.stdin.readlines()
 
