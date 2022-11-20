72ce1a49c96a7b84918dc0bede4b70da4e203712
Vedrrox <225344@edu.p.lodz.pl>
2022-11-20 11:33:35 +0100

    konwersja

--- lab5/main.py
+++ lab5/main.py
@@ -4,13 +4,37 @@ import sys
 def print_digits(format_string,index):
     buffer=""
     for i in range (index,len(format_string)):
-        if format_string[i].isdigit():
-            buffer += format_string[i]
-        elif format_string[i]!=" ":
-            return ""
+        
+        if (format_string[i]=='9'):
+            format_string[i]=='8'
+
+        elif (format_string[i]=='8'):
+            format_string[i]=='7' 
+
+        elif (format_string[i]=='7'):
+            format_string[i]=='6' 
+
+        elif (format_string[i]=='6'):
+            format_string[i]=='5' 
+
+        elif (format_string[i]=='5'):
+            format_string[i]=='4' 
+
+        elif (format_string[i]=='4'):
+            format_string[i]=='3'
+        elif (format_string[i]=='3'):
+            format_string[i]=='2' 
+
+        elif (format_string[i]=='2'):
+            format_string[i]=='1' 
+        elif (format_string[i]=='1'):
+            format_string[i]=='9'  
+        
         else:
             return buffer[::-1]   
     return ""
+
+    
 
 
 def my_printf(format_string,param):
@@ -19,9 +43,9 @@ def my_printf(format_string,param):
     buffer=""
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1]=='g':
+            if format_string[idx] == '#' and format_string[idx+1]=='X' and format_string[idx+3]=='g':
                 print(param,end="")
-                buffer=print_digits(format_string,idx+3)
+                buffer=print_digits(format_string,idx+4)
                 if(buffer!=""):
                     print(" "+buffer,end="")
                 shouldDo=False     
@@ -31,6 +55,9 @@ def my_printf(format_string,param):
         else:
             buffer==""
             shouldDo=True
+
+    print_digits(format_string,0)
+             
     print("")
 
 
@@ -38,5 +65,5 @@ def my_printf(format_string,param):
 
 data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for idx in range(0,len(data),2):
+    my_printf(data[idx].rstrip(),data[idx+1].rstrip())
