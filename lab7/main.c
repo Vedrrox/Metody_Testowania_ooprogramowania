388cc6d5541f115a9df8ee56e8a01cf23f64a082
Vedrrox <225344@edu.p.lodz.pl>
2022-12-18 11:07:48 +0100

    zmiany w kodzie

--- lab7/main.c
+++ lab7/main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+
+void change_to_hex(char *str1)  {
+	int i, len, temp;
+    len = strlen(str1);
+    for (i = 0; i < len; i++) {
+        
+    }
+}
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+		if((format_string[i] == '#') && (format_string[i+1] == 'j')){
 			i++;
+
+			int paramIsNumber = 1;
+            for (int j = 0; j < strlen(param); j++) {
+				if (!isdigit(param[j])) {
+					paramIsNumber = 0;
+					break;
+				}
+            }
+
+			if (paramIsNumber) {
+				change_to_hex(param);
+			} else {
+				puts("");
+				return 0;
+			}
+			
+
+
+
 			printf("%s",param);
 		}else
 			putchar(format_string[i]);
@@ -15,7 +43,6 @@ int my_printf(char *format_string, char *param){
 
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
-	
 	while(gets(buf)){
 		gets(buf2);
 		my_printf(buf,buf2);
