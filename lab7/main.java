cbd2aecb466b2c22a58c7f2f83debf8b9ae1500c
Vedrrox <225344@edu.p.lodz.pl>
2022-12-18 11:35:38 +0100

    zmana na java

---
+++ lab7/main.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+
+class lab0 {
+	public static void my_printf(String format_string, String param){
+		for(int i=0;i<format_string.length();i++){
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
+				System.out.print(param);
+				i++;
+			}else{
+				System.out.print(format_string.charAt(i));
+			}
+		}
+		System.out.println("");
+	}
+	public static void main(String[] args) throws IOException {
+		//System.out.println("Hello, World!"); 
+		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
+		//Scanner sc=new Scanner(System.in);
+		String format_string, param;
+		while(bufferReader.ready()) {
+			format_string=bufferReader.readLine();
+			param=bufferReader.readLine();
+			my_printf(format_string,param);
+		}
+	}
+}
