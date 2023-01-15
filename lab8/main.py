f418b4a981f7e9156ecc297eb3a069865abd4e8d
Vedrrox <225344@edu.p.lodz.pl>
2023-01-15 10:55:48 +0100

    zad

--- lab8/main.py
+++ lab8/main.py
@@ -2,31 +2,31 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
+def my_printf(format_string, param):
     shouldDo=True
     if len(format_string) > 0:
         if shouldDo:
-            positionj = format_string.find("#j")
-            positionJ = format_string.find("#J")
-            if positionj != -1 or positionJ != -1:
-                positionhasz = format_string.find("#")
-                positionOfNumber = positionhasz + 2
+            positionZ = format_string.find("#.Z")
+            if positionZ != -1:
+                positionOfNumber = positionZ + 3
                 NumberString = ""
                 StringNumber = ""
-                for i in range(positionOfNumber,len(format_string)):
-                        StringNumber += format_string[i]
-                StringNumberInt = int(StringNumber)
-                NumberInHex = hex(StringNumberInt)
-                HexNumberToString = str(NumberInHex)
-                HexNumberToString= HexNumberToString.replace("a", "g")
-                HexNumberToString= HexNumberToString.replace("b", "h")
-                HexNumberToString= HexNumberToString.replace("c", "i")
-                HexNumberToString= HexNumberToString.replace("d", "j")
-                HexNumberToString= HexNumberToString.replace("e", "k")
-                HexNumberToString= HexNumberToString.replace("f", "l")
-                HexNumberToString = HexNumberToString.replace("0", "o")
-                print(HexNumberToString)
+                for i in range(positionOfNumber, len(format_string)):
+                    if format_string[i].isnumeric():
+                        NumberString += format_string[i]
+                    else:
+                        break
+                StringNumberInt = int(NumberString)
+                NumberInHex = hex(param)
+                NumberInHex= NumberInHex[2:].rjust(StringNumberInt,'0')
+                NumberInHex= NumberInHex.replace("a", "g")
+                NumberInHex= NumberInHex.replace("b", "h")
+                NumberInHex= NumberInHex.replace("c", "i")
+                NumberInHex= NumberInHex.replace("d", "j")
+                NumberInHex= NumberInHex.replace("e", "k")
+                NumberInHex= NumberInHex.replace("f", "l")
+                NumberInHex= NumberInHex.replace("0", "o")
+                print(NumberInHex)
                 shouldDo=False
             else:
                 print(format_string)
@@ -35,7 +35,7 @@ def my_printf(format_string,param):
     else:
         print("String is empty")
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), int(data[i+1].rstrip()))
